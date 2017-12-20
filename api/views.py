7abb38fe5c85a01b5a13ae8e563c341124720572
Jhon Adrián <jadrian.ceron@gmail.com>
2017-12-20 10:41:22 -0500

    Se incluye el método de autenticación por defecto

--- api/views.py
+++ api/views.py
@@ -5,7 +5,8 @@ import os, re, sys
 
 import pickle
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 
@@ -19,6 +20,7 @@ from spell_checking import SpellTweet, _to_str
 
 
 @api_view(['POST'])
+@permission_classes((IsAuthenticated, ))
 def spell_checking(request):
     """Vista principal de la API.
 
